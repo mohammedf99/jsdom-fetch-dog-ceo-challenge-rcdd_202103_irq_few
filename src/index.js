e77fd291ea6e2d6ec4f07731c236f7e0b1e6a856
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-04-16 12:57:51 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,65 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgURL = 'https://dog.ceo/api/breeds/image/random/4';
+const BREED_URL = 'https://dog.ceo/api/breeds/list/all';
+
+document.addEventListener('DOMContentLoaded', function(){
+  
+  fetch(imgURL).then(function(response){
+    // console.log(response.json());
+    return response.json();
+    
+  }).then(function(json){
+    
+    displayImage(json);
+    
+  });
+  
+  
+  // fetch(BREED_URL).then(function(response){
+  //   return response.json();
+  // }).then(function(json){
+  //   addBreeds(json);
+  // });
+  
+  
+  
+});
+
+
+function displayImage(json){
+  
+  const imgsContainer = document.querySelector("#dog-image-container");
+  
+  
+  for(let i = 0; i < json.message.length; i++){
+    
+    const div = document.createElement('div');
+    div.innerHTML = `<img src="${json.message[i]}" alt="dog-image">`;
+    imgsContainer.append(div);
+  }
+}
+
+
+// function addBreeds(json){
+  
+//   breedNames = Object.key(r.message);
+//   const breedUl = document.querySelector('#dog-breeds');
+  
+//   for(let i = 0; i < breedNames.length; i++){
+    
+//     const breedLi = document.createElement('li');
+  
+//     breedLi.innerHTML = `<img src="${json[i]}" alt="image breed">`;
+    
+//     breedUl.appendChild(breedLi);
+//   }
+  
+// }
+
+
+
+
+
+
+
